e0a8783351929819f0068546cbc0365a8514f41f
qtux <qtux@users.noreply.github.com>
2016-01-29 23:53:01 +0100

    implemented tilemap

--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
 window.onload = function() {
-	var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
+	var game = new Phaser.Game(1152, 720, Phaser.AUTO, '', { preload: preload, create: create });
+	var map;
+	var layer;
+	
 	function preload () {
-		game.load.image('logo', 'test.png');
+		console.log('starting preload()');
+		game.load.tilemap('map', 'assets/tilemaps/test.json', null, Phaser.Tilemap.TILED_JSON);
+		game.load.image('tileset', 'assets/tilesets/basictiles.png');
+		console.log('preload() done');
 	}
+	
 	function create () {
-		var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-		logo.anchor.setTo(0.5, 0.5);
+		console.log('starting create()');
+		// load tile map
+		game.stage.backgroundColor = '#555555';
+		map = game.add.tilemap('map');
+		map.addTilesetImage('basictiles', 'tileset');
+		layer = map.createLayer('layerfloor');
+		layer.resizeWorld();
+		
+		// scaling
+		game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+		game.input.onDown.add(gofull, this);
+		
+		console.log('create() done');
+	}
+	
+	function gofull() {
+		if (game.scale.isFullScreen) {
+			game.scale.stopFullScreen();
+		}
+		else {
+			game.scale.startFullScreen(false);
+		}
 	}
 };
