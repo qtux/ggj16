28de9b17a291f4424ede086e3f50f35c46b95aaf
qtux <qtux@users.noreply.github.com>
2016-01-30 02:25:05 +0100

    added collision

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 window.onload = function() {
-	var game = new Phaser.Game(1152, 720, Phaser.AUTO, '', { preload: preload, create: create });
+	var game = new Phaser.Game(1152, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update});
 	var map;
 	var layer;
+	var player;
 	
 	function preload () {
 		console.log('starting preload()');
 		game.load.tilemap('map', 'assets/tilemaps/test.json', null, Phaser.Tilemap.TILED_JSON);
 		game.load.image('tileset', 'assets/tilesets/basictiles.png');
+		game.load.spritesheet('player', 'assets/spritesheets/hero.png', 36, 72);
 		console.log('preload() done');
 	}
 	
@@ -18,6 +20,19 @@ window.onload = function() {
 		map.addTilesetImage('basictiles', 'tileset');
 		layer = map.createLayer('layer0');
 		layer.resizeWorld();
+		
+		// add player sprite
+		player = game.add.sprite(200, 200, 'player', 1);
+		game.physics.startSystem(Phaser.Physics.P2JS);
+		map.setCollision(100, true, "collision");
+		//Then the physics engine creates collision bodies from the tiles:
+		game.physics.p2.convertTilemap(map, "collision");
+		game.physics.p2.enable(player);
+		player.body.fixedRotation = true; // no rotation
+		//player.body.addRectangle(72, 36, 0, 25);	// only lower part collides
+		//Controls
+		cursors = game.input.keyboard.createCursorKeys();
+		//player.body.debug = true;
 		
 		// scaling
 		game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
@@ -32,6 +47,24 @@ window.onload = function() {
 		}
 		else {
 			game.scale.startFullScreen(false);
+		}
+	}
+	
+	function update() {
+		var speed = 300;
+		if (cursors.left.isDown){
+			player.body.velocity.x = -speed;
+		} else if (cursors.right.isDown){
+			player.body.velocity.x = speed;
+		} else {
+			player.body.velocity.x = 0;
+		}
+		if (cursors.up.isDown){
+			player.body.velocity.y = -speed;
+		} else if (cursors.down.isDown){
+			player.body.velocity.y = speed;
+		} else {
+			player.body.velocity.y = 0;
 		}
 	}
 };
