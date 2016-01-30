1bb2973f18a2ae93cb6a91f27358caf9ba66de1b
qtux <qtux@users.noreply.github.com>
2016-01-30 04:17:53 +0100

    added sheep (collision not working at the moment)

--- src/main.js
+++ src/main.js
@@ -1,49 +1,65 @@
 window.onload = function() {
 	var game = new Phaser.Game(1152, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update});
-	var map;
-	var layer;
-	var layer1;
-	//var layer2;
-	var player;
 	
+	// game ressources
+	var map, layer, layer1;		// tilemap related
+	var player, sheep;			// sprites
+	var npcCG;					// collision groups
+	
+	/**
+	 * preload - load assets
+	 */
 	function preload () {
-		console.log('starting preload()');
 		game.load.tilemap('map', 'assets/tilemaps/test.json', null, Phaser.Tilemap.TILED_JSON);
 		game.load.image('tileset', 'assets/tilesets/basictiles.png');
 		game.load.spritesheet('player', 'assets/spritesheets/hero.png', 36, 72);
-		console.log('preload() done');
+		game.load.spritesheet('sheep', 'assets/spritesheets/sheep.png', 36, 36);
 	}
 	
+	/**
+	 * create - generate and initialise game content
+	 */
 	function create () {
-		console.log('starting create()');
+		// enable scaling
+		game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+		game.input.onDown.add(gofull, this);
+		
+		// start physics system
+		game.physics.startSystem(Phaser.Physics.P2JS);
+		
+		// register collision groups
+		npcCG = game.physics.p2.createCollisionGroup();
+		
 		// load tile map
 		game.stage.backgroundColor = '#555555';
 		map = game.add.tilemap('map');
 		map.addTilesetImage('basictiles', 'tileset');
 		layer = map.createLayer('layer0');
 		layer1 = map.createLayer('layer1');
-		//layer2 = map.createLayer('layer2');
 		layer.resizeWorld();
-		
-		// add player sprite
-		player = game.add.sprite(200, 200, 'player', 1);
-		game.physics.startSystem(Phaser.Physics.P2JS);
+		// set collision using the collision map
 		map.setCollision(100, true, "collision");
-		//Then the physics engine creates collision bodies from the tiles:
 		game.physics.p2.convertTilemap(map, "collision");
+		
+		// add sprites
+		player = game.add.sprite(200, 200, 'player', 1);
+		//player.body.debug = true;
+		sheep = game.add.sprite(400, 200, 'sheep');
+		
+		// enable physics for player
 		game.physics.p2.enable(player);
 		player.body.fixedRotation = true; // no rotation
 		player.body.collideWorldBounds = true;
-		//player.body.addRectangle(72, 36, 0, 25);	// only lower part collides
-		//Controls
+		//player.body.collides(npcCG);
+		
+		// enable physics for sheep
+		game.physics.p2.enable(sheep);
+		sheep.body.setCollisionGroup(npcCG);
+		sheep.body.collides(player);
+		sheep.body.collideWorldBounds = true;
+		
+		// enable user input
 		cursors = game.input.keyboard.createCursorKeys();
-		//player.body.debug = true;
-		
-		// scaling
-		game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
-		game.input.onDown.add(gofull, this);
-		
-		console.log('create() done');
 	}
 	
 	function gofull() {
@@ -57,16 +73,16 @@ window.onload = function() {
 	
 	function update() {
 		var speed = 300;
-		if (cursors.left.isDown){
+		if (cursors.left.isDown) {
 			player.body.velocity.x = -speed;
-		} else if (cursors.right.isDown){
+		} else if (cursors.right.isDown) {
 			player.body.velocity.x = speed;
 		} else {
 			player.body.velocity.x = 0;
 		}
-		if (cursors.up.isDown){
+		if (cursors.up.isDown) {
 			player.body.velocity.y = -speed;
-		} else if (cursors.down.isDown){
+		} else if (cursors.down.isDown) {
 			player.body.velocity.y = speed;
 		} else {
 			player.body.velocity.y = 0;
