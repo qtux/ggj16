431284de5160b91894d969989b8ed0552ec9ea25
qtux <qtux@users.noreply.github.com>
2016-01-31 17:49:16 +0100

    added release.html to be used as index.html file in a release

--- createUpload.sh
+++ createUpload.sh
@@ -9,7 +9,8 @@ mkdir -p archive/src
 
 # copy
 cp screenshots/*.png archive/press
-cp -R index.html favicon.ico src/ assets/ archive/src
+cp -R favicon.ico src/ assets/ archive/src
+cp release.html archive/src/index.html
 cp LICENSE archive/license.txt
 echo "In order to start the game start a webserver inside the source folder and open index.html." > archive/src/readme.txt
 echo "A live version is avaiable at http://qtux.github.io/ggj16/." >> archive/src/readme.txt
